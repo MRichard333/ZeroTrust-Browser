75458b76e9f3206f82aaaabb6a16cdc8d92c5727
Matthieu Richard <mr3@mrichard333.com>
2026-03-22 04:52:53 -0400

    Optimized Performance for old hardare and stability fix 10

--- autoconfig.js
+++ autoconfig.js
@@ -1,6 +1,5 @@
-// ZeroTrust Browser — autoconfig.js
+// ZeroTrust Browser — autoconfig.js v2.4.0
 // Deploy to: <firefox-install>/defaults/pref/autoconfig.js
-// This tells Firefox to load mozilla.cfg from the installation directory.
-// Must NOT have a blank first line — comment on line 1 is fine.
+// Tells Firefox to load mozilla.cfg from the installation directory.
 pref("general.config.filename", "mozilla.cfg");
 pref("general.config.obscure_value", 0);
